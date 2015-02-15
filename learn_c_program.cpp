a78383c3ec01cd6391ddf71475b3c4ec7bd03459
liu kang <krliukang@163.com>
2015-02-15 11:44:50 +0800

    Degrees centigrade thermometer

---
+++ learn_c_program.cpp
@@ -0,0 +1,35 @@
+// learn_c_program.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+/*
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+*/
+
+
+int main()
+{
+	int n;
+	
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n",fahr,celsius);
+		fahr = fahr +step;
+	}
+
+	scanf("%d",&n);
+}
